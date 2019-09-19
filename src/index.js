1d2f0f6df190a406202f0b3974b959a3d9003986
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-09-19 23:39:34 +0300

    first simple version of adaptive components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+var posthtml = require('express-posthtml');
+
+const repos = require('./api/repos');
 
 const app = express();
-const repos = require('./api/repos');
+
 
 if (process.argv.length <= 2 || !fs.existsSync(process.argv[2])) {
   console.log("There is should be path of an existed directory in args");
@@ -12,7 +16,28 @@ if (process.argv.length <= 2 || !fs.existsSync(process.argv[2])) {
 process.env.DIR = process.argv[2];
 console.log("Seted process.env.DIR: " + process.env.DIR);
 
+const plugins = [
+  require('posthtml-include')({ root: path.join(__dirname , '/views')}),
+  require('posthtml-bem')(),
+  require('posthtml-expressions')({ locals: { 
+    title: 'ala', 
+    pathArray: ['dir', 'dirr', 'filedir', 'file']
+  }}),
+]
+const options = {}
+
+app.set('views', path.join(__dirname , '/views'));
+app.set('view options', { plugins: plugins, options: options })
+
+app.use('/static', express.static(__dirname + '/static'));
 app.use('/api/repos', repos);
+
+app.engine('html', posthtml)
+
+
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');

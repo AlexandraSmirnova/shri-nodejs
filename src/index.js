1107522b3e54f5d4d73fcea1dcb15cc30ea5b246
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-09-22 00:47:14 +0300

    add fileDescription, dropdown, link and some fixes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const app = express();
 
 
 if (process.argv.length <= 2 || !fs.existsSync(process.argv[2])) {
-  console.log("There is should be path of an existed directory in args");
-  process.exit(-1);
+    console.log("There is should be path of an existed directory in args");
+    process.exit(-1);
 }
 
 process.env.DIR = process.argv[2];
@@ -18,15 +18,15 @@ console.log("Seted process.env.DIR: " + process.env.DIR);
 
 const TEMPLATE_DIR = path.join(__dirname, '/views');
 const plugins = [
-  require('posthtml-extend')({ root: TEMPLATE_DIR }),
-  require('posthtml-include')({ root: TEMPLATE_DIR }),
-  require('posthtml-bem')(),
-  require('posthtml-expressions')({
-    locals: {
-      title: 'ala',
-      pathArray: ['dir', 'dirr', 'filedir', 'file']
-    }
-  }),
+    require('posthtml-extend')({ root: TEMPLATE_DIR }),
+    require('posthtml-include')({ root: TEMPLATE_DIR }),
+    require('posthtml-bem')(),
+    require('posthtml-expressions')({
+        locals: {
+            title: 'ala',
+            pathArray: ['dir', 'dirr', 'filedir', 'file'],
+        }
+    }),
 ]
 const options = {}
 
@@ -40,32 +40,32 @@ app.engine('html', posthtml)
 
 
 app.get('/', function (req, res) {
-  res.render('pages/index.html');
+    res.render('pages/index.html');
 });
 
 app.get('/branches', function (req, res) {
-  res.render('pages/branches.html');
+    res.render('pages/branches.html');
 });
 
 app.get('/commit', function (req, res) {
-  res.render('pages/commit.html');
+    res.render('pages/commit.html');
 });
 
 app.get('/file', function (req, res) {
-  res.render('pages/file.html', {
-    plugins: [
-      ...plugins,
-      require('posthtml-expressions')({
-        locals: {
-          title: 'ala',
-          file: JSON.parse(testFile),
-        }
-      }),
-    ]
-  });
+    res.render('pages/file.html', {
+        plugins: [
+            ...plugins,
+            require('posthtml-expressions')({
+                locals: {
+                    title: 'ala',
+                    file: JSON.parse(testFile),
+                }
+            }),
+        ]
+    });
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
 
